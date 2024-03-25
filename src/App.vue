56874873653714dee3171aceb4b07cb13bbc68bf
txstc55 <txstc55@gmail.com>
2024-03-24 23:38:13 -0700

    done?

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <FaceGenerator />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FaceGenerator from './views/FaceGenerator.vue'
 
 export default {
+  components: { FaceGenerator },
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
@@ -20,7 +17,5 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
