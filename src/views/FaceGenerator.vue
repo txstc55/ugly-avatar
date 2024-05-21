9c1532ecb43d3e2af8e36ef16ffc6e68ceb5dbae
txstc55 <txstc55@gmail.com>
2024-05-21 02:14:25 -0700

    they sometimes look tired

--- src/views/FaceGenerator.vue
+++ src/views/FaceGenerator.vue
@@ -125,8 +125,9 @@
           :points="eyeRightUpper"
           fill="none"
           stroke="black"
-          :stroke-width="3.0 / faceScale"
+          :stroke-width="(this.haventSleptForDays ? 5.0 : 3.0) / faceScale"
           stroke-linejoin="round"
+          stroke-linecap="round"
           filter="url(#fuzzy)"
         />
         <polyline
@@ -134,8 +135,9 @@
           :points="eyeRightLower"
           fill="none"
           stroke="black"
-          :stroke-width="4.0 / faceScale"
+          :stroke-width="(this.haventSleptForDays ? 5.0 : 3.0) / faceScale"
           stroke-linejoin="round"
+          stroke-linecap="round"
           filter="url(#fuzzy)"
         />
         <circle
@@ -146,7 +148,7 @@
           :cy="rightPupilShiftY + Math.random() * 5 - 2.5"
           stroke="black"
           fill="none"
-          :stroke-width="1.0"
+          :stroke-width="1.0 + Math.random() * 0.5"
           filter="url(#fuzzy)"
           clip-path="url(#rightEyeClipPath)"
         />
@@ -165,7 +167,7 @@
           :points="eyeLeftUpper"
           fill="none"
           stroke="black"
-          :stroke-width="4.0 / faceScale"
+          :stroke-width="(this.haventSleptForDays ? 5.0 : 3.0) / faceScale"
           stroke-linejoin="round"
           filter="url(#fuzzy)"
         />
@@ -174,7 +176,7 @@
           :points="eyeLeftLower"
           fill="none"
           stroke="black"
-          :stroke-width="4.0 / faceScale"
+          :stroke-width="(this.haventSleptForDays ? 5.0 : 3.0) / faceScale"
           stroke-linejoin="round"
           filter="url(#fuzzy)"
         />
@@ -186,7 +188,7 @@
           :cy="leftPupilShiftY + Math.random() * 5 - 2.5"
           stroke="black"
           fill="none"
-          :stroke-width="1.0"
+          :stroke-width="1.0 + Math.random() * 0.5"
           filter="url(#fuzzy)"
           clip-path="url(#leftEyeClipPath)"
         />
@@ -198,7 +200,7 @@
           :points="hair"
           fill="none"
           :stroke="hairColor"
-          :stroke-width="2"
+          :stroke-width="0.5 + Math.random() * 2.5"
           stroke-linejoin="round"
           filter="url(#fuzzy)"
         />
@@ -213,7 +215,7 @@
             :cy="rightNoseCenterY + Math.random() * 4 - 2"
             stroke="black"
             fill="none"
-            :stroke-width="1.0"
+            :stroke-width="1.0 + Math.random() * 0.5"
             filter="url(#fuzzy)"
           />
         </g>
@@ -226,7 +228,7 @@
             :cy="leftNoseCenterY + Math.random() * 4 - 2"
             stroke="black"
             fill="none"
-            :stroke-width="1.0"
+            :stroke-width="1.0 + Math.random() * 0.5"
             filter="url(#fuzzy)"
           />
         </g>
@@ -249,7 +251,7 @@
           "
           fill="none"
           stroke="black"
-          :stroke-width="3"
+          :stroke-width="2.5 + Math.random() * 1.0"
           stroke-linejoin="round"
           filter="url(#fuzzy)"
         ></path>
@@ -259,7 +261,7 @@
           :points="mouthPoints"
           fill="rgb(215,127,140)"
           stroke="black"
-          :stroke-width="3"
+          :stroke-width="2.7 + Math.random() * 0.5"
           stroke-linejoin="round"
           filter="url(#fuzzy)"
         />
@@ -313,6 +315,7 @@ export default {
       leftNoseCenterX: 0, // the center of the left nose
       leftNoseCenterY: 0, // the center of the left nose
       hairs: [],
+      haventSleptForDays: false,
       hairColors: [
         "rgb(0, 0, 0)", // Black
         "rgb(44, 34, 43)", // Dark Brown
@@ -323,7 +326,7 @@ export default {
         "rgb(165, 42, 42)", // Red
         "rgb(145, 85, 61)", // Auburn
         "rgb(128, 128, 128)", // Grey
-        "rgb(185, 55, 55)", // fire
+        "rgb(185, 55, 55)", // Fire
         "rgb(255, 192, 203)", // Pastel Pink
         "rgb(255, 105, 180)", // Bright Pink
         "rgb(230, 230, 250)", // Lavender
@@ -334,7 +337,74 @@ export default {
         "rgb(255, 165, 0)", // Vivid Orange
         "rgb(220, 20, 60)", // Crimson Red
         "rgb(192, 192, 192)", // Silver
+        "rgb(255, 215, 0)", // Gold
+        "rgb(255, 255, 255)", // White
+        "rgb(124, 252, 0)", // Lawn Green
+        "rgb(127, 255, 0)", // Chartreuse
+        "rgb(0, 255, 127)", // Spring Green
+        "rgb(72, 209, 204)", // Medium Turquoise
+        "rgb(0, 255, 255)", // Cyan
+        "rgb(0, 206, 209)", // Dark Turquoise
+        "rgb(32, 178, 170)", // Light Sea Green
+        "rgb(95, 158, 160)", // Cadet Blue
+        "rgb(70, 130, 180)", // Steel Blue
+        "rgb(176, 196, 222)", // Light Steel Blue
+        "rgb(30, 144, 255)", // Dodger Blue
+        "rgb(135, 206, 235)", // Sky Blue
+        "rgb(0, 0, 139)", // Dark Blue
+        "rgb(138, 43, 226)", // Blue Violet
+        "rgb(75, 0, 130)", // Indigo
+        "rgb(139, 0, 139)", // Dark Magenta
+        "rgb(153, 50, 204)", // Dark Orchid
+        "rgb(186, 85, 211)", // Medium Orchid
+        "rgb(218, 112, 214)", // Orchid
+        "rgb(221, 160, 221)", // Plum
+        "rgb(238, 130, 238)", // Violet
+        "rgb(255, 0, 255)", // Magenta
+        "rgb(216, 191, 216)", // Thistle
+        "rgb(255, 20, 147)", // Deep Pink
+        "rgb(255, 69, 0)", // Orange Red
+        "rgb(255, 140, 0)", // Dark Orange
+        "rgb(255, 165, 0)", // Orange
+        "rgb(250, 128, 114)", // Salmon
+        "rgb(233, 150, 122)", // Dark Salmon
+        "rgb(240, 128, 128)", // Light Coral
+        "rgb(205, 92, 92)", // Indian Red
+        "rgb(255, 99, 71)", // Tomato
+        "rgb(255, 160, 122)", // Light Salmon
+        "rgb(220, 20, 60)", // Crimson
+        "rgb(139, 0, 0)", // Dark Red
+        "rgb(178, 34, 34)", // Fire Brick
+        "rgb(250, 235, 215)", // Antique White
+        "rgb(255, 239, 213)", // Papaya Whip
+        "rgb(255, 235, 205)", // Blanched Almond
+        "rgb(255, 222, 173)", // Navajo White
+        "rgb(245, 245, 220)", // Beige
+        "rgb(255, 228, 196)", // Bisque
+        "rgb(255, 218, 185)", // Peach Puff
+        "rgb(244, 164, 96)", // Sandy Brown
+        "rgb(210, 180, 140)", // Tan
+        "rgb(222, 184, 135)", // Burly Wood
+        "rgb(250, 250, 210)", // Light Goldenrod Yellow
+        "rgb(255, 250, 205)", // Lemon Chiffon
+        "rgb(255, 245, 238)", // Sea Shell
+        "rgb(253, 245, 230)", // Old Lace
+        "rgb(255, 228, 225)", // Misty Rose
+        "rgb(255, 240, 245)", // Lavender Blush
+        "rgb(250, 240, 230)", // Linen
+        "rgb(255, 228, 181)", // Moccasin
+        "rgb(255, 250, 250)", // Snow
+        "rgb(240, 255, 255)", // Azure
+        "rgb(240, 255, 240)", // Honeydew
+        "rgb(245, 245, 245)", // White Smoke
+        "rgb(245, 255, 250)", // Mint Cream
+        "rgb(240, 248, 255)", // Alice Blue
+        "rgb(240, 248, 255)", // Ghost White
+        "rgb(248, 248, 255)", // Ghost White
+        "rgb(255, 250, 240)", // Floral White
+        "rgb(253, 245, 230)", // Old Lace
       ],
+
       hairColor: "black",
       dyeColorOffset: "50%",
       backgroundColors: [
@@ -358,12 +428,51 @@ export default {
         "rgb(0, 100, 0)", // Dark Green
         "rgb(0, 0, 139)", // Dark Blue
         "rgb(105, 105, 105)", // Dim Gray
+        "rgb(240, 128, 128)", // Light Coral
+        "rgb(255, 160, 122)", // Light Salmon
+        "rgb(255, 218, 185)", // Peach Puff
+        "rgb(255, 228, 196)", // Bisque
+        "rgb(255, 222, 173)", // Navajo White
+        "rgb(255, 250, 205)", // Lemon Chiffon
+        "rgb(250, 250, 210)", // Light Goldenrod Yellow
+        "rgb(255, 239, 213)", // Papaya Whip
+        "rgb(255, 245, 238)", // Sea Shell
+        "rgb(255, 248, 220)", // Cornsilk
+        "rgb(255, 255, 240)", // Ivory
+        "rgb(240, 255, 240)", // Honeydew
+        "rgb(240, 255, 255)", // Azure
+        "rgb(240, 248, 255)", // Alice Blue
+        "rgb(248, 248, 255)", // Ghost White
+        "rgb(255, 250, 250)", // Snow
+        "rgb(255, 240, 245)", // Lavender Blush
+        "rgb(255, 228, 225)", // Misty Rose
+        "rgb(230, 230, 250)", // Lavender
+        "rgb(216, 191, 216)", // Thistle
+        "rgb(221, 160, 221)", // Plum
+        "rgb(238, 130, 238)", // Violet
+        "rgb(218, 112, 214)", // Orchid
+        "rgb(186, 85, 211)", // Medium Orchid
+        "rgb(147, 112, 219)", // Medium Purple
+        "rgb(138, 43, 226)", // Blue Violet
+        "rgb(148, 0, 211)", // Dark Violet
+        "rgb(153, 50, 204)", // Dark Orchid
+        "rgb(139, 69, 19)", // Saddle Brown
+        "rgb(160, 82, 45)", // Sienna
+        "rgb(210, 105, 30)", // Chocolate
+        "rgb(205, 133, 63)", // Peru
+        "rgb(244, 164, 96)", // Sandy Brown
+        "rgb(222, 184, 135)", // Burly Wood
+        "rgb(255, 250, 240)", // Floral White
+        "rgb(253, 245, 230)", // Old Lace
+        "rgb(250, 240, 230)", // Linen
       ],
       mouthPoints: [],
     };
   },
   methods: {
     generateFace() {
+      this.faceScale = 1.5 + Math.random() * 0.6;
+      this.haventSleptForDays = Math.random() > 0.8;
       let faceResults = faceShape.generateFaceCountourPoints();
       this.computedFacePoints = faceResults.face;
       this.faceHeight = faceResults.height;
@@ -384,44 +493,44 @@ export default {
         .concat(left.lower.slice(10, 90).reverse());
       this.distanceBetweenEyes = randomFromInterval(
         this.faceWidth / 4.5,
-        this.faceWidth / 4
+        this.faceWidth / 4,
       );
       this.eyeHeightOffset = randomFromInterval(
         this.faceHeight / 8,
-        this.faceHeight / 6
+        this.faceHeight / 6,
       );
       this.leftEyeOffsetX = randomFromInterval(
         -this.faceWidth / 20,
-        this.faceWidth / 10
+        this.faceWidth / 10,
       );
       this.leftEyeOffsetY = randomFromInterval(
         -this.faceHeight / 50,
-        this.faceHeight / 50
+        this.faceHeight / 50,
       );
       this.rightEyeOffsetX = randomFromInterval(
         -this.faceWidth / 20,
-        this.faceWidth / 10
+        this.faceWidth / 10,
       );
       this.rightEyeOffsetY = randomFromInterval(
         -this.faceHeight / 50,
-        this.faceHeight / 50
+        this.faceHeight / 50,
       );
       this.leftEyeCenter = left.center[0];
       this.rightEyeCenter = right.center[0];
       this.leftPupilShiftX = randomFromInterval(
         -this.faceWidth / 20,
-        this.faceWidth / 20
+        this.faceWidth / 20,
       );
 
       // now we generate the pupil shifts
       // we first pick a point from the upper eye lid
       let leftInd0 = Math.floor(randomFromInterval(10, left.upper.length - 10));
       let rightInd0 = Math.floor(
-        randomFromInterval(10, right.upper.length - 10)
+        randomFromInterval(10, right.upper.length - 10),
       );
       let leftInd1 = Math.floor(randomFromInterval(10, left.upper.length - 10));
       let rightInd1 = Math.floor(
-        randomFromInterval(10, right.upper.length - 10)
+        randomFromInterval(10, right.upper.length - 10),
       );
       let leftLerp = randomFromInterval(0.2, 0.8);
       let rightLerp = randomFromInterval(0.2, 0.8);
@@ -448,41 +557,41 @@ export default {
       if (Math.random() > 0.3) {
         this.hairs = hairLines.generateHairLines0(
           this.computedFacePoints,
-          numHairLines[0] * 1 + 10
+          numHairLines[0] * 1 + 10,
         );
       }
       if (Math.random() > 0.3) {
         this.hairs = this.hairs.concat(
           hairLines.generateHairLines1(
             this.computedFacePoints,
-            numHairLines[1] / 1.5 + 10
-          )
+            numHairLines[1] / 1.5 + 10,
+          ),
         );
       }
       if (Math.random() > 0.5) {
         this.hairs = this.hairs.concat(
           hairLines.generateHairLines2(
             this.computedFacePoints,
-            numHairLines[2] * 3 + 10
-          )
+            numHairLines[2] * 3 + 10,
+          ),
         );
       }
       if (Math.random() > 0.5) {
         this.hairs = this.hairs.concat(
           hairLines.generateHairLines3(
             this.computedFacePoints,
-            numHairLines[3] * 3 + 10
-          )
+            numHairLines[3] * 3 + 10,
+          ),
         );
       }
       this.rightNoseCenterX = randomFromInterval(
         this.faceWidth / 18,
-        this.faceWidth / 12
+        this.faceWidth / 12,
       );
       this.rightNoseCenterY = randomFromInterval(0, this.faceHeight / 5);
       this.leftNoseCenterX = randomFromInterval(
         -this.faceWidth / 18,
-        -this.faceWidth / 12
+        -this.faceWidth / 12,
       );
       this.leftNoseCenterY =
         this.rightNoseCenterY +
@@ -500,19 +609,19 @@ export default {
         this.mouthPoints = mouthShape.generateMouthShape0(
           this.computedFacePoints,
           this.faceHeight,
-          this.faceWidth
+          this.faceWidth,
         );
       } else if (choice == 1) {
         this.mouthPoints = mouthShape.generateMouthShape1(
           this.computedFacePoints,
           this.faceHeight,
-          this.faceWidth
+          this.faceWidth,
         );
       } else {
         this.mouthPoints = mouthShape.generateMouthShape2(
           this.computedFacePoints,
           this.faceHeight,
-          this.faceWidth
+          this.faceWidth,
         );
       }
     },
